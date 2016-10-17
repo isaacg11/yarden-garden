f53a7f0e1ad66deaa72e5886ca4bb227c0646fa1
isaacg11 <isaacgreywebdeveloper@gmail.com>
2016-10-16 20:42:31 -0700

    added smooth scroll

--- ngApp/app.ts
+++ ngApp/app.ts
@@ -1,37 +1,48 @@
 namespace MyApp {
 
-    angular.module('app', ['ui.router', 'ngResource', 'ui.bootstrap']).config((
+    angular.module('app', ['ui.router', 'ngResource', 'ui.bootstrap', 'duScroll']).config((
         $stateProvider: ng.ui.IStateProvider,
         $urlRouterProvider: ng.ui.IUrlRouterProvider,
-        $locationProvider: ng.ILocationProvider
+        $locationProvider: ng.ILocationProvider,
     ) => {
+
         // Define routes
         $stateProvider
             .state('Home', {
                 url: '/',
                 templateUrl: '/ngApp/views/home.html',
                 controller: MyApp.Controllers.HomeController,
-                controllerAs: 'controller'
+                controllerAs: 'vm'
             })
             .state('About', {
                 url: '/about',
-                templateUrl: '/ngApp/views/about.html'
+                templateUrl: '/ngApp/views/about.html',
+                controller: MyApp.Controllers.HomeController,
+                controllerAs: 'vm'
             })
             .state('Careers', {
                 url: '/careers',
-                templateUrl: '/ngApp/views/careers.html'
+                templateUrl: '/ngApp/views/careers.html',
+                controller: MyApp.Controllers.HomeController,
+                controllerAs: 'vm'
             })
             .state('Pricing', {
                 url: '/pricing',
-                templateUrl: '/ngApp/views/pricing.html'
+                templateUrl: '/ngApp/views/pricing.html',
+                controller: MyApp.Controllers.HomeController,
+                controllerAs: 'vm'
             })
             .state('Login', {
                 url: '/login',
-                templateUrl: '/ngApp/views/login.html'
+                templateUrl: '/ngApp/views/login.html',
+                controller: MyApp.Controllers.HomeController,
+                controllerAs: 'vm'
             })
             .state('notFound', {
                 url: '/notFound',
-                templateUrl: '/ngApp/views/notFound.html'
+                templateUrl: '/ngApp/views/notFound.html',
+                controller: MyApp.Controllers.HomeController,
+                controllerAs: 'vm'
             });
 
         // Handle request for non-existent route
--- ngApp/controllers/controllers.ts
+++ ngApp/controllers/controllers.ts
@@ -1,9 +1,29 @@
 namespace MyApp.Controllers {
 
-// landing page views
+// landing pages
     export class HomeController {
+      public login;
 
-        constructor() {
+      public goToSignup() {
+        this.$rootScope.$broadcast('signup', {value: 'get-started'});
+      }
+
+      public constructor(
+        private $scope: ng.IScope,
+        private $rootScope: ng.IRootScopeService,
+        private $state: ng.ui.IStateService,
+        public $window: ng.IWindowService,
+        public $document
+      ) {
+
+          $window.scrollTo(0, 0);
+
+          $scope.$on('signup', function(event, data) {
+            $state.go('Home').then(() => {
+              let el = angular.element(document.getElementById('get-started'));
+              $document.scrollToElement(el);
+            })
+          });
         }
-    }
+      }
 }
